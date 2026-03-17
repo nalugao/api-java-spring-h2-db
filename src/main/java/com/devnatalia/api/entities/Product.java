587e73dc3849434b854ca3e338966a93c488020a
nalugao <nalugao@hotmail.com>
2026-03-17 16:27:54 -0300

    Product entity

--- src/main/java/com/devnatalia/api/entities/Product.java
+++ src/main/java/com/devnatalia/api/entities/Product.java
@@ -1,4 +1,51 @@
 package com.devnatalia.api.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_product")
 public class Product {
+
+    @Id
+    private UUID id;
+    private String name;
+    private Double price;
+    private Integer quantity;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
 }
+
